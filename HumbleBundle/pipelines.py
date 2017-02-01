fcdb59933d74eddddd49dbe617e17858ecee6591
SeokYoung Kim <crom@zzizily.com>
2017-02-02 06:31:03 +0900

    REDIS 접속 정보 있어서 다시 지금 올림

---
+++ HumbleBundle/pipelines.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
+from HumbleBundle.settings import pool
+import redis
+
+class HumblebundlePipeline(object):
+    def process_item(self, item, spider):
+        r = redis.Redis(connection_pool=pool)
+        url = item['url']
+        if url and url.split('//'):
+            key = ':'.join(url.split('//')[-1].split('/'))
+        else:
+            key = url
+        r.lpush(key, item)
+        return item
