4404c5ed591d5a792e0241d728b53cea1371f8d0
SeokYoung Kim <crom@zzizily.com>
2017-02-16 00:40:12 +0900

    redis key 값 변경 https://www.humblebundle.com/pc-lovers-software-bundle url 에 2단계에 특별한 url 이 없을때

--- bta/pipelines.py
+++ bta/pipelines.py
@@ -13,7 +13,10 @@ class BtaPipeline(object):
         r = redis.Redis(connection_pool=pool)
         url = item['url']
         if url and url.split('//'):
-            key = ':'.join(url.split('//')[-1].split('/'))
+            if 2 == len(url.split('//')[-1].split('/')):
+                key = 'top:'.join(url.split('//')[-1].split('/'))
+            else:
+                key = ':'.join(url.split('//')[-1].split('/'))
         else:
             key = url
         r.lpush('bta_price:{}'.format(key), json.dumps(item))
