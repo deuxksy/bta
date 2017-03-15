d3553c44d83008b27ae1948d0212b25bc806c8f4
SeokYoung Kim <crom@zzizily.com>
2017-03-15 15:50:17 +0900

    file template 추가

--- bta/__init__.py
+++ bta/__init__.py
@@ -1,3 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+
+"""
+
+__title__ = 'bta'
+__author__ = 'SeokYoung Kim<crom@zzizily.com>'
+__status__ = 'develoment'
+__version__ = '0.0.1'
+__date__ = '2017-03-11'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2017 SeokYoung Kim'
+
 import os
 
 from cryptography.fernet import Fernet
