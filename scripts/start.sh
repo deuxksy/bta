9635ebb9794138ee0a73e9c599a4233ecde5b79a
SeokYoung Kim <crom@zzizily.com>
2017-03-16 04:17:29 +0900

    script 수정

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
+export ZZIZILY_KIKI_CRYPTO=X_AEu2y68Q91Hmf4ooi7vUKkT5msSMvDEHDcVp-EFEI=
 export ZZIZILY_BTA_MODE=dev
 export ZZIZILY_BTA_HOME=${HOME}/apps/bta
 export ZZIZILY_BTA_CRYPTO=tLZIcdBwFUx8KImvo1OuOQIy4WQoc5kf8QvyR_xiCZM=
-export ZZIZILY_KIKI_CRYPTO=X_AEu2y68Q91Hmf4ooi7vUKkT5msSMvDEHDcVp-EFEI=
 
 cd ${ZZIZILY_BTA_HOME}
 
